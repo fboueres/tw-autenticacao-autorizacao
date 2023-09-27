f9849e2aa3852181589ee69bed2844258c5704d0
fernandoboueres <fernandoboueres.ti@gmail.com>
2023-09-27 17:34:01 -0300

    15 - Definindo rotas protegidas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AutheticationController;
 use App\Http\Controllers\HomePage;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', HomePage::class)->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', HomePage::class)->name('home');
+    Route::post('/logout', [AutheticationController::class, 'logout'])->name('logout');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,4 +30,3 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register')
 
 Route::get('/login', [AutheticationController::class, 'login'])->name('login.form');
 Route::post('/login', [AutheticationController::class, 'logar'])->name('login');
-Route::post('/logout', [AutheticationController::class, 'logout'])->name('logout');
